e1467b24f09c92a7eb5dd5314df9ee246158fdba
Diego Barboza <diegojose1994@gmail.com>
2025-03-19 22:08:04 -0400

    More comments have been added to the HomeView component explaining the new logic

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -7,13 +7,17 @@ import ShowTableButton from "../components/common/ShowTableButton";
 // The Header, LaunchesTable, and Footer components are created to modularize the code, which is the purpose of react, dividing everything into smaller components.
 
 const HomeView = () => {
+  // A state is created to manage the visibility of the LaunchesTable component
   const [showTable, setShowTable] = useState(false);
 
+  // The handleClick function is created to change the value of the showTable state to true when the button is clicked.
+  // the void type is declared since it is a function that receives no arguments and returns nothing
   const handleClick = (): void => {
     setShowTable(true);
   };
 
   return (
+    // A ternary operator is used to evaluate showTable, when it is true the contents of the section are displayed and when it is false the button is displayed
     <>
       {showTable ? (
         <>
