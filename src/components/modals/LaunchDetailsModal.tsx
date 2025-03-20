3c3f896b354e8bbd9ef8c3abf662340227ccaa3e
Diego Barboza <diegojose1994@gmail.com>
2025-03-20 19:30:36 -0400

    LaunchDetailsModal has been optimized by adding more details and styling.

--- src/components/modals/LaunchDetailsModal.tsx
+++ src/components/modals/LaunchDetailsModal.tsx
@@ -1,4 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
+import dayjs from "dayjs";
 
 interface LaunchDetailsModal {
   launchId: string;
@@ -11,11 +12,17 @@ const LaunchDetailsModal = ({ launchId, setShowModal }: LaunchDetailsModal) => {
       launch(id: $id) {
         id
         mission_name
+        launch_date_utc
         rocket {
           rocket_name
           rocket_type
         }
         details
+        links {
+          article_link
+          video_link
+          flickr_images
+        }
       }
     }
   `;
@@ -33,10 +40,63 @@ const LaunchDetailsModal = ({ launchId, setShowModal }: LaunchDetailsModal) => {
 
   return (
     <div className="flex justify-center items-center fixed inset-0 bg-[rgba(0,0,0,.85)] z-10">
-      <div className="w-[80%] h-[80vh] bg-white rounded-md">
-        <div className="flex justify-between items-center p-8">
-          <h1 className="font-bold text-3xl">{launch.mission_name}</h1>
-          <button className="cursor-pointer" onClick={() => setShowModal(false)}>❌</button>
+      <button
+        onClick={() => setShowModal(false)}
+        className="bg-white rounded-full absolute top-5 right-5 md:top-7 md:right-9 p-2 text-md cursor-pointer shadow md:shadow-none"
+      >
+        ❌
+      </button>
+      <div className="w-full md:w-[80%] h-screen md:h-[80vh] bg-white rounded-md p-8 overflow-y-auto">
+        <hgroup className="mb-4">
+          <h1 className="font-bold text-3xl mb-1">{launch.mission_name}</h1>
+          <h2 className="text-xl font-semibold mb-2">{`${launch.rocket.rocket_name} / ${launch.rocket.rocket_type}`}</h2>
+          <h3>{`Launch Date: ${dayjs(launch.launch_date_utc).format(
+            "YYYY-MM-DD HH:mm"
+          )}`}</h3>
+        </hgroup>
+
+        {launch.links.flickr_images.length >= 1 ? (
+          <div className="grid grid-cols-[repeat(auto-fill,minmax(10em,1fr))] gap-4 mb-4">
+            {launch.links.flickr_images.map((url: string) => {
+              return (
+                <img
+                  src={url}
+                  alt="image-launch"
+                  className="w-full aspect-square object-cover"
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <p className="mb-4 text-red-500">There are no images for this launch.</p>
+        )}
+
+        <p className="mb-8">
+          {launch.details || "There are no details for this launch."}
+        </p>
+
+        <div className="flex flex-col md:flex-row gap-4">
+          {launch.links.article_link && (
+            <a
+              href={launch.links.article_link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-gray-800 hover:bg-gray-950 text-white rounded-md py-2 px-6 cursor-pointer"
+            >
+              Read article
+            </a>
+          )}
+
+          {launch.links.video_link && (
+            <a
+              href={launch.links.video_link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-gray-800 hover:bg-gray-950 text-white rounded-md py-2 px-6 cursor-pointer"
+            >
+              Watch video
+            </a>
+          )}
         </div>
       </div>
     </div>
