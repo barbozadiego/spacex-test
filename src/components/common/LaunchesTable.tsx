5ca89711231a48aba5dc2bc17060278f4c5364bf
Diego Barboza <diegojose1994@gmail.com>
2025-03-19 22:22:44 -0400

    Bonus: dayjs is installed to format release dates and display them in a user-friendly way.

--- src/components/common/LaunchesTable.tsx
+++ src/components/common/LaunchesTable.tsx
@@ -1,4 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
+import dayjs from "dayjs";
 
 // The GetLaunches constant is created, which will contain the query
 // The gql function is used to process the request text and execute the query.
@@ -40,39 +41,49 @@ const LaunchesTable = () => {
 
   // A basic table is returned with the request data.
   return (
-   
-
-      <div className="h-[70vh] overflow-auto rounded-md border border-gray-900">
-        <table className="w-full bg-white">
-          <thead className="sticky top-0 z-10">
-            <tr className="bg-gray-950 text-gray-200">
-              <th className="py-2 px-4 text-left whitespace-nowrap">ID</th>
-              <th className="py-2 px-4 text-left whitespace-nowrap">LAUNCH DATE</th>
-              <th className="py-2 px-4 text-left whitespace-nowrap">MISSION NAME</th>
-              <th className="py-2 px-4 text-left whitespace-nowrap">MISSION ID</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* All the launches are mapped, but first we make sure that "data"
+    <div className="h-[70vh] overflow-auto rounded-md border border-gray-900">
+      <table className="w-full bg-white">
+        <thead className="sticky top-0 z-10">
+          <tr className="bg-gray-950 text-gray-200">
+            <th className="py-2 px-4 text-left whitespace-nowrap">ID</th>
+            <th className="py-2 px-4 text-left whitespace-nowrap">
+              LAUNCH DATE
+            </th>
+            <th className="py-2 px-4 text-left whitespace-nowrap">
+              MISSION NAME
+            </th>
+            <th className="py-2 px-4 text-left whitespace-nowrap">
+              MISSION ID
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* All the launches are mapped, but first we make sure that "data"
           exists using "?" to avoid possible errors such as undefined or null,
           because this data is obtained through a request, it is not immediately
           available and by not using the "?" symbol we will be trying to access
           a nested value that will not be available, which will give us an
           error. */}
-            {data?.launches?.map((launch: Launch, index) => (
-              // Here it is declared that each element of the array is of type Launch.
-              <tr key={launch.id} className="border-b border-gray-900">
-                {/* I also wanted to show the index to show the table more ordered with the numbering. */}
-                <td className="py-2 px-4">{index + 1}</td>
-                {/* It is worth noting that we use "+ 1" since indexing an array starts at 0 */}
-                <td className="py-2 px-4 whitespace-nowrap">{launch.launch_date_utc}</td>
-                <td className="py-2 px-4 whitespace-nowrap">{launch.mission_name}</td>
-                <td className="py-2 px-4 whitespace-nowrap">{launch.mission_id}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          {data?.launches?.map((launch: Launch, index) => (
+            // Here it is declared that each element of the array is of type Launch.
+            <tr key={launch.id} className="border-b border-gray-900">
+              {/* I also wanted to show the index to show the table more ordered with the numbering. */}
+              <td className="py-2 px-4">{index + 1}</td>
+              {/* It is worth noting that we use "+ 1" since indexing an array starts at 0 */}
+              <td className="py-2 px-4 whitespace-nowrap">
+                {dayjs(launch.launch_date_utc).format("YYYY-MM-DD HH:mm")}
+              </td>
+              <td className="py-2 px-4 whitespace-nowrap">
+                {launch.mission_name}
+              </td>
+              <td className="py-2 px-4 whitespace-nowrap">
+                {launch.mission_id}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 export default LaunchesTable;
